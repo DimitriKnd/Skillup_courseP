80f4f592157b22f1dbc861b5cc20fab3580ae189
DimitriKnd <dmytro133@gmail.com>
2020-12-14 17:58:03 +0200

    Lesson_2_2

---
+++ Calc.py
@@ -0,0 +1,23 @@
+a = 1
+b = 2
+c = 3
+
+print("a+b =", a+b)
+print("b+c =", b+c)
+print("a+c =", a+c)
+
+print("a-b =", a-b)
+print("b-c =", b-c)
+print("a-c =", a-c)
+
+print("a*b =", a*b)
+print("b*c =", b*c)
+print("a*c =", a*c)
+
+print("a/b =", a/b)
+print("b/c =", b/c)
+print("a/c =", a/c)
+
+print("a^b =", a**b)
+print("b^c =", b**c)
+print("a^c =", a**c)
